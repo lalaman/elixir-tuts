c7d25a5f3bbc5a6ebd00162af326646ddf9555f8
lalaman <lalaman@live.ca>
2017-02-04 02:05:20 -0500

    Completed identicon module.

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,94 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  Process:
+    `String
+    |> Computer MD5 hash of string
+    |> List of #'s based on string
+    |> Choose colour
+    |> Build grid of squares
+    |> Convert grid to image
+    |> Save image`
+  """
+
+  @doc """
+  Hello world
+  """
+  def main(input) do
+     input
+     |> hash_input
+     |> pick_colour
+     |> build_grid
+     |> filter_odd_squares
+     |> build_pixel_map
+     |> draw_image
+     |> save_image(input)
+  end
+
+  def save_image(image, filename) do
+    File.write("pngs/#{filename}.png", image);
+  end
+
+  def draw_image(%Identicon.Image{ colour: colour, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(colour)
+
+    Enum.each(pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map(grid, fn({_code, index}) ->
+      x = rem(index, 5) * 50
+      y = div(index, 5) * 50
+
+      top_left = {x, y}
+      bottom_right = {x + 50, y + 50}
+
+      {top_left, bottom_right}
+    end)
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+     filtered_grid = Enum.filter(grid, fn({code, _index}) ->
+       rem(code, 2) == 0
+     end)
+
+     %Identicon.Image{image | grid: filtered_grid}
+  end
+
+  def pick_colour(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
+     %Identicon.Image{image | colour: {r,g,b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1) # pass reference to that function
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+    #  for [a,b,c] <- hex do
+    #    [a,b,c,b,a]
+    #  end
+  end
+
+  # turns string into md5 hash, then into a list of those numbers
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
